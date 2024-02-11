ac8547fdb1323b2df7d00e0c5685463ab681f460
shashank.sanket014@gmail.com <shashank.sanket014@gmail.com>
2024-02-11 19:02:22 +0530

    feat: add Piechart component and stories

---
+++ src/app/components/PieChart.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useDrawingArea } from '@mui/x-charts/hooks';
+import { styled } from '@mui/material/styles';
+
+const data = [
+  { value: 5 },
+  { value: 10 },
+  { value: 15 },
+  { value: 20 },
+];
+
+const size = {
+  width: 300,
+  height: 200,
+};
+
+const StyledText = styled('text')(({ theme, bold }) => ({
+  fill: theme.palette.text.primary,
+  textAnchor: 'middle',
+  dominantBaseline: 'central',
+  fontSize: bold?30:20,
+  fontWeight: bold ? 'bold' : 'normal', // Apply bold font weight conditionally
+}));
+
+function PieCenterLabel({ title, label }: PieProps) {
+  const { width, height, left, top } = useDrawingArea();
+  return (
+    <>
+      <StyledText x={left + width / 2} y={top + height / 2-10} bold>
+        {title}
+      </StyledText>
+      <StyledText x={left + width / 2} y={top + height / 2+30}>
+        {label}
+      </StyledText>
+    </>
+  );
+}
+
+interface PieProps {
+  title: string;
+  label: string;
+}
+
+const Pie = ({ title, label, ...props }: PieProps) => {
+  return (
+    <PieChart
+      series={[
+        {
+          data,
+          innerRadius: 90,
+          outerRadius: 100,
+          paddingAngle: 0,
+          cornerRadius: 1,
+          startAngle: -180,
+          endAngle: 180,
+        },
+      ]}
+      {...size}
+    >
+      <PieCenterLabel title={title} label={label} />
+    </PieChart>
+  );
+};
+
+export default Pie;
---
+++ src/stories/Pie.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Pie from '../app/components/PieChart';
+
+const meta = {
+  title: 'Components/Pie',
+  component: Pie,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Pie>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: 'Title',
+    label: 'Label'
+  },
+};
+
