b89aa077a1be58dfe607952e07ce87ea7e532635
shashank.sanket014@gmail.com <shashank.sanket014@gmail.com>
2024-02-11 20:02:32 +0530

    feat: fix on table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,8 +6,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import edit from "../../assets/edit.svg"
-import delet from "../../assets/delete.svg"
+import edit from "../assets/edit.svg"
+import delet from "../assets/delete.svg"
 
 function createData(
   Name: string,
